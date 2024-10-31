fe933e5f3ca24fc1a4189e6d3b437eedb750e3aa
BRuslanB <rbaidyussenov@gmail.com>
2024-10-31 23:08:52 +0500

    Add new folders and files

---
+++ notification-service/src/main/java/kz/bars/wellify/notification_service/aop/TransactionAspect.java
@@ -0,0 +1,20 @@
+//package kz.bars.wellify.notification_service.aop;
+//
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Before;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Aspect
+//@Component
+//public class TransactionAspect {
+//
+//    @Before("@annotation(transactional)")
+//    public void beginTransaction(Transactional transactional) {
+//        // Дополнительные действия перед началом транзакции, если требуется
+//        // Например, логирование или инициализация транзакционных ресурсов
+//    }
+//
+//    // Обратите внимание, что Spring самостоятельно завершает транзакции.
+//    // Здесь могут быть дополнительные настройки для логирования транзакций.
+//}
---
+++ storage-service/src/main/java/kz/bars/wellify/storage_service/aop/ErrorHandlingAspect.java
@@ -0,0 +1,19 @@
+package kz.bars.wellify.storage_service.aop;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ErrorHandlingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandlingAspect.class);
+
+    @AfterThrowing(pointcut = "execution(* kz.bars.wellify.storage_service..*(..))", throwing = "ex")
+    public void logException(Exception ex) {
+        logger.error("Exception caught: " + ex.getMessage(), ex);
+    }
+}
